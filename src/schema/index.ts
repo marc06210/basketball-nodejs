d25fa644faa86f27ad036d1232e04fa41f3e86b2
Marc Guerrini <maguerrini@airfrance.fr>
2021-10-27 16:27:37 +0200

    database configuration

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,5 @@
 import { GraphQLObjectType, GraphQLSchema } from 'graphql';
-import player from './queries/players';
+import player from './queries/player';
 import hello from './queries/hello';
 
 // see https://github.com/juffalow/express-graphql-example/blob/master/src/schema/index.ts
--- src/server.ts
+++ src/server.ts
@@ -9,23 +9,18 @@ import Context from "./context/context";
 
 createConnection({
     type: "postgres",
-    host: process.env["DB_HOSTNAME"], //"localhost",
-    port: Number(process.env["DB_PORT"]), //14300,
-    username: process.env["DB_USERNAME"], //"basketball",
-    password: process.env["DB_PASSWORD"],//"password",
-    database: process.env["DB_NAME"],//"basketball",
+    host: process.env["DB_HOSTNAME"],
+    port: Number(process.env["DB_PORT"]),
+    username: process.env["DB_USERNAME"],
+    password: process.env["DB_PASSWORD"],
+    database: process.env["DB_NAME"],
     entities: [
-        //Player
         __dirname + "/entities/*.js"
     ],
     synchronize: true,
     logging: true
 }).then(connection => {
     // here you can start to work with your entities
-    let me = new Player();
-    me.firstname = "Marc";
-    me.lastname = "G."
-
     let playerRepository = connection.getRepository<Player>(Player)
     let context: Context = {
         repositories: {
@@ -39,7 +34,6 @@ createConnection({
             graphqlHTTP({
                 context: context,
                 schema,
-                // rootValue: root,
                 graphiql: true
             })
     );
@@ -47,11 +41,6 @@ createConnection({
     app.listen(PORT, () => {
         console.log("Graphql server up and running on port " + PORT)
     });
-    // return playerRepository
-    //         .save(me)
-    //         .then(me => {
-    //             console.log("I have been added as a player. My id is", me.id);
-    //         });
 }).catch(error => console.log(error));
 
 //express initialization
