5ba5470de925b060384ac25d7240ff09c80a15d2
Marc Guerrini <maguerrini@airfrance.fr>
2021-10-27 17:37:18 +0200

    formating

--- src/schema/types/player.ts
+++ src/schema/types/player.ts
@@ -7,31 +7,31 @@ import {
   
 import Player from 'src/entities/player';
   
-  const player = new GraphQLObjectType({
-    name: 'Player',
-    fields: () => ({
-      id: {
-        type: GraphQLNonNull(GraphQLID),
-        description: 'Database id of the player',
-        resolve: (p: Player): number => {
-          return p.id;
-        }
-      },
-      lastName: {
-        type: GraphQLNonNull(GraphQLString),
-        description: 'Last name of the player',
-        resolve: (p: Player): string => {
-          return p.lastname;
-        }
-      },
-      firstName: {
-        type: GraphQLNonNull(GraphQLString),
-        description: 'First name of the player',
-        resolve: (p: Player): string => {
-          return p.firstname;
-        },
+const player = new GraphQLObjectType({
+  name: 'Player',
+  fields: () => ({
+    id: {
+      type: GraphQLNonNull(GraphQLID),
+      description: 'Database id of the player',
+      resolve: (p: Player): number => {
+        return p.id;
       }
-    })
-  });
-  
-  export default player;
+    },
+    lastName: {
+      type: GraphQLNonNull(GraphQLString),
+      description: 'Last name of the player',
+      resolve: (p: Player): string => {
+        return p.lastname;
+      }
+    },
+    firstName: {
+      type: GraphQLNonNull(GraphQLString),
+      description: 'First name of the player',
+      resolve: (p: Player): string => {
+        return p.firstname;
+      },
+    }
+  })
+});
+
+export default player;
