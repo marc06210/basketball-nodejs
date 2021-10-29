40f6b862780d880142955bd7ac8b7bc3d9a2aa01
Marc Guerrini <maguerrini@airfrance.fr>
2021-10-29 15:36:49 +0200

    import path updated

--- src/schema/queries/players.ts
+++ src/schema/queries/players.ts
@@ -1,6 +1,6 @@
 import { GraphQLList } from 'graphql';
-import Context from 'src/context/context';
-import Player from 'src/entities/player';
+import Context from '../../context/context';
+import Player from '../../entities/player';
 import { default as playerType } from '../types/player';
 
 export default {
